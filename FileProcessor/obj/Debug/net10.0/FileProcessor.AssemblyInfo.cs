85461cd3a4b7437568d6c1d38870a653bd654186
TakeItEasy-Working <liuyu.working@gmail.com>
2026-02-04 16:22:16 +0800

    Before Move to Desktop

--- FileProcessor/obj/Debug/net10.0/FileProcessor.AssemblyInfo.cs
+++ FileProcessor/obj/Debug/net10.0/FileProcessor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileProcessor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1e787fecae2651f2db42cd1ea8cf2a62c32ac40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9349b17f59fe5dc4ebde0205e5d3cbf758173c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileProcessor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileProcessor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
